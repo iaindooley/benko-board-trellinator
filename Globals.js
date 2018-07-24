1d3214f19c6e034031246cce5117b3a3e60e948a
Iain Dooley <iain@workingsoftware.com.au>
2018-07-24 14:26:05 +1000

    Don't ignore unexpected exceptions, post reply to split if checklist not found

--- Globals.js
+++ Globals.js
@@ -4,17 +4,9 @@ function checkOffArchivedCard(notification)
     var notif = new Notification(notification);
 
 /* When a card with label "HoZ Linked Card" is archived find the first card linked in the attachments and check item "{boardname}: {triggercardlink}" */
-    try
-    {
-        var arched = notif.archivedCard();
-        arched.label(new RegExp("(Benko|HoZ) Linked Card"));
-        arched.cardsLinkedInAttachments().first().checkItemByName(new RegExp(".*: "+notif.card().link()));
-    }
-    
-    catch(e)
-    {
-        writeInfo_("Did not check off archived card: "+e);
-    }
+    var arched = notif.archivedCard();
+    arched.label(new RegExp("(Benko|HoZ) Linked Card"));
+    arched.cardsLinkedInAttachments().first().checkItemByName(new RegExp(".*: "+notif.card().link()));
 }
 
 /* add comment to card */
@@ -23,43 +15,35 @@ function copyComment(notification)
 /* When a comment is added to a card with label "HoZ Linked Card" find the first card linked in the attachments and add a comment "{username} said: {commenttext}" */
     var notif = new Notification(notification);
     
+    var comment = notif.commentAddedToCard();
+    notif.card().label(new RegExp("(Benko|HoZ) Linked Card"));
+
     try
     {
-        var comment = notif.commentAddedToCard();
-        notif.card().label(new RegExp("(Benko|HoZ) Linked Card"));
-
-        try
+        notif.card().cardsLinkedInAttachments().each(function(card)
         {
-            notif.card().cardsLinkedInAttachments().each(function(card)
+            try
             {
-                try
-                {
-                    card.label("Benko Master");
+                card.label("Benko Master");
 
-                    card.checklist("Linked Cards").items().each(function(item)
-                    {
-                        if(item.name().indexOf(notif.card().link()) > -1)
-                            throw card;
-                    });
-                }
-                
-                catch(e)
+                card.checklist("Linked Cards").items().each(function(item)
                 {
-                    Notification.expectException(InvalidDataException,e);
-                }
-            });
-        }
-        
-        catch(e)
-        {
-            Notification.expectException(Card,e);
-            e.postComment(notif.member().name()+" said: "+comment.text().replace("@",""));
-        }
+                    if(item.name().indexOf(notif.card().link()) > -1)
+                        throw card;
+                });
+            }
+            
+            catch(e)
+            {
+                Notification.expectException(InvalidDataException,e);
+            }
+        });
     }
     
     catch(e)
     {
-        writeInfo_(e);
+        Notification.expectException(Card,e);
+        e.postComment(notif.member().name()+" said: "+comment.text().replace("@",""));
     }
 }
 
@@ -70,21 +54,11 @@ function checkOffSplittedCard(notification)
     var split_regex = "Split (.*)";
     
 /* When a card with label "Split {*}" is archived, find the card mentioned in the description and check item "{triggercardlink}" in checklist "{labelwildcard1}" */
-    try
-    {
-        if(
-            (arch = notif.archivedCard()) && 
-            (arch.label(new RegExp(split_regex)))
-          )
-        {
-            arch.cardLinkedInDescription().checkItemByName(notif.card().link());
-        }
-    }
-
-    catch(e)
-    {
-        writeInfo_("Did not archive splitted card: "+e);
-    }
+    if(
+        (arch = notif.archivedCard()) && 
+        (arch.label(new RegExp(split_regex)))
+      )
+        arch.cardLinkedInDescription().checkItemByName(notif.card().link());
 }
 
 /* add label to card */
@@ -92,30 +66,22 @@ function bucket(notification)
 {
     var notif = new Notification(notification);
 /* When the label "Bucket" is added to a card remove the "Bucket" label and collect cards in list "{triggercardname}" into linked items in checklist "Sub-tasks" and archive list "{triggercardname}" */
-    try
-    {
-        var label = notif.labelAddedToCard("Bucket");
-        var trig = notif.card();
-        trig.removeLabel(label);
-        var list = notif.board().list(trig.name());
+    var label = notif.labelAddedToCard("Bucket");
+    var trig = notif.card();
+    trig.removeLabel(label);
+    var list = notif.board().list(trig.name());
 
-        list.cards().each(function(loop)
+    list.cards().each(function(loop)
+    {
+        trig.addChecklist("Sub-tasks",function(list)
         {
-            trig.addChecklist("Sub-tasks",function(list)
-            {
-                list.addItem(loop.link());
-            });
-            
-            loop.archive();
+            list.addItem(loop.link());
         });
         
-        list.archive();
-    }
-
-    catch(e)
-    {
-        writeInfo_(e);
-    }
+        loop.archive();
+    });
+    
+    list.archive();
 }
 
 /* add label to card */
@@ -126,36 +92,29 @@ function split(notification)
 convert the checklist "{labelwildcard1}" into linked cards at the bottom of list "{triggercardname} Split: {labelwildcard1}"
 copying labels and members and find card with id "{triggercardid}" and remove label "Split {labelwildcard1}" */
 
-    try
-    {
-        var added_label = notif.labelAddedToCard();
-        var board = notif.board();
+    var added_label = notif.labelAddedToCard();
+    var board = notif.board();
 
-        if(parts = new RegExp("Split (.*)").exec(added_label.name()))
+    if(parts = new RegExp("Split (.*)").exec(added_label.name()))
+    {
+        try
         {
-            try
-            {
-                var cl = notif.card().checklist(parts[1])
-                var list = board.findOrCreateList(notif.card().name()+" Split: "+parts[1]);
-                var params = {
-                              idLabels: notif.card().labels().transform(function(label)
-                                        {
-                                            return label.data.id;
-                                        }).implodeValues(",")
-                             };
-                cl.convertIntoLinkedCards(list,params);
-                notif.card().removeLabel(added_label);
-            }
-            
-            catch(e)
-            {
-                writeInfo_("Checklist not found for: "+parts[1]+" "+e);
-            }
+            var cl = notif.card().checklist(parts[1])
+            var list = board.findOrCreateList(notif.card().name()+" Split: "+parts[1]);
+            var params = {
+                          idLabels: notif.card().labels().transform(function(label)
+                                    {
+                                        return label.data.id;
+                                    }).implodeValues(",")
+                         };
+            cl.convertIntoLinkedCards(list,params);
+            notif.card().removeLabel(added_label);
         }
-    }
-
-    catch(e)
-    {
-        writeInfo_(e);
+        
+        catch(e)
+        {
+            Notification.exceptionException(InvalidDataException,e);
+            notif.replyToMember("There was no checklist named: "+parts[1]);
+        }
     }
 }
